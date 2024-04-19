f6679e341d0435a26d82b5c952d2f9a1a4a43fda
LilAtza <ysaacnoe.correa@unmsm.edu.pe>
2024-04-18 21:08:00 -0500

    ✨ (MenuItem): create component MenuItem

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,14 @@
+import type { MenuItem } from '../types'
+
+type MenuItemProps = {
+  item: MenuItem
+}
+
+export default function MenuItem( {item}: MenuItemProps ) {
+  return (
+    <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between">
+      <p>{item.name}</p>
+      <p className="font-black">{item.price}</p>
+    </button>
+  );
+}
